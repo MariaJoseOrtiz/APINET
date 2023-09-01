8a06341d7a9b3cc9ef512fbf16e9407a7a8a3d7e
Maria Ortiz <ortizmarij94@gmail.com>
2023-09-01 10:50:09 -0400

    Api en c# basica

---
+++ Conexion/ConexionBD.cs
@@ -0,0 +1,24 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace TiendaApi.Conexion
+{
+    public class ConexionBD
+    {
+        private string connectionString = string.Empty;
+        public ConexionBD()
+        {
+            var constructor = new ConfigurationBuilder().SetBasePath
+                (Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json").Build();
+
+            connectionString = constructor.GetSection("ConnectionStrings:conexionmaestra").Value;
+
+        }
+
+        public string cadenaSQL()
+        {
+            return connectionString;
+        }
+
+
+    }
+}
